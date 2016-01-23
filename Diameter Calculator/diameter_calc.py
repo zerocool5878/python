de79ef2a3c80cd69c2d8a2139c630d7f58aa9e54
zerocool5878 <zerocool5878@gmail.com>
2016-01-23 10:50:37 -0500

    spelling errors

--- Diameter Calculator/diameter_calc.py
+++ Diameter Calculator/diameter_calc.py
@@ -1,11 +1,11 @@
 from fractions import Fraction
 
-print "Do you wish to calculate the Diameter or Radius from a perimeter measurement? Type 'd' or 'r'"
+print "Do you wish to calculate the Diameter or Radius from a circumference measurement? Type 'd' or 'r'"
 var = raw_input("Please type 'd' or 'r'")
 
 
 if var == 'd':
-	print "Now type the perimeter measurement for the diameter you would like calculate."
+	print "Now type the circumference measurement for the diameter you would like calculate."
 	cir = float(raw_input())
 	answer = cir/3.1415926535
 	print "What accuracy do you want the diameter rounded to. Type 8 for nearest 1/8, 16 for 1/16th etc."
@@ -14,13 +14,13 @@ if var == 'd':
 	frac = Fraction(int(rnd), denominator)
 	if rnd < denominator:
 		print frac
-		print "The diameter of a %s perimeter is" % cir, frac
+		print "The diameter of a %s circumference is" % cir, frac
 	else:
 		remain = rnd%denominator
 		whole_num = int(rnd/denominator)
-		print "The diameter of a %s perimeter is" % cir, whole_num, Fraction(int(remain), denominator)
+		print "The diameter of a %s circumference is" % cir, whole_num, Fraction(int(remain), denominator)
 elif var == 'r':
-	print "Now type the perimeter measurement for the radius you would like calculate."
+	print "Now type the circumference measurement for the radius you would like calculate."
 	cir = float(raw_input())
 	answer = cir/3.1415926535
 	print "What accuracy do you want the diameter rounded to. Type 8 for nearest 1/8, 16 for 1/16th etc."
@@ -29,10 +29,10 @@ elif var == 'r':
 	frac = Fraction(int(rnd), denominator)
 	if rnd < denominator:
 		print frac
-		print "The radius of a %s perimeter is" % cir, frac
+		print "The radius of a %s circumference is" % cir, frac
 	else:
 		remain = rnd%denominator
 		whole_num = int(rnd/denominator)
-		print "The radius of a %s perimeter is" % cir, whole_num, Fraction(int(remain), denominator)
+		print "The radius of a %s circumference is" % cir, whole_num, Fraction(int(remain), denominator)
 else:
 	print "Error: You did not type 'd' or 'r' Please run again"
